9cfbc4bc34526d6893d48b8ea02c1909896492f6
ektarathi <rathiekta2801@gmail.com>
2020-12-03 16:11:21 +0000

    Updated the List Data Component
    Added Moment.js package and updated the styling of list data.

--- src/components/ListData.tsx
+++ src/components/ListData.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import moment from 'moment';
+
 export interface ListDataProps {
   items: any;
 }
@@ -9,9 +11,14 @@ const ListData: React.SFC<ListDataProps> = ({ items }: ListDataProps) => {
       <ul className="section-content__list">
         {items.map((item: any, index: any) => (
           <li className="section-content__list-item" key={index}>
-            <div>{`# ${item.flight_number}`}</div>
-            <div>{item.mission_name}</div>
-            <div>{item.launch_date_utc}</div>
+            <div className="section-content__list-id">{`#${item.flight_number}`}</div>
+            <div className="section-content__list-name">
+              {item.mission_name}
+            </div>
+            <div className="section-content__list-data">
+              <div className="section-content__list-data section-content__list-data--date">{moment(item.launch_date_utc).format("Do MMM YYYY")}</div>
+              <div className="section-content__list-data section-content__list-data--name">{item.rocket.rocket_name}</div>
+            </div>
           </li>
         ))}
       </ul>
